30714c1f57fd71b162f856fefd54efb622ce49cc
Andrew <andrew@rebekas-macbook-air.home>
2022-07-20 23:24:05 +0200

    initial commit

---
+++ turtletest.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+
+color('green')
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+right(90)
+forward(200)
+
+
+
+getscreen()._root.mainloop()
+
+# bye()
